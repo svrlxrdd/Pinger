4850ff69adc7ed672a336fa7986b950a9012dd65
sv.1337 <70086639+svrlxrdd@users.noreply.github.com>
2020-11-28 07:32:53 +0530

    Add files via upload

---
+++ pinger.py
@@ -0,0 +1,257 @@
+import os, sys, time #Sleep, sending command prompt cmds for pinging, sys because why not
+from colorama import init, Fore #Colors
+
+#def clear
+def clear():
+    os.system('cls')
+
+def movementart():
+    art1 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@               -----  ----
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         \    -
+                                                     -    '-.
+                                                      '.    /
+                                                        '-./
+    """
+
+    art2 = r"""
+        .-----------------TTTT-----__
+                                                 /''''''''''(__O] ----------  __/]
+                              ...---'''"_ --''                                     __@ -----   ---- 
+                              !'''                   .   __=---------""'''''''''''''''
+                              !                ..--''!   l L !_l   !
+                              !          ..--''      .  /-_j '   '
+                              !    ..--''           /  ,       '   '
+                              !--''                /           `    \
+                                                   L'          \    -
+                                                                -    '-.
+                                                                 '.    /
+                                                                   '-./
+    """
+
+    art3 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                                                    \    -
+                                                     -    '-.
+                                                      '.    /
+                                                        '-./
+    """
+
+    art4 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                                                    
+                                                    \    -
+                                                     -    '-.
+                                                      '.    /
+                                                        '-./
+    """
+
+    art5 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                                                    
+                                                           -.
+                                                      '.    /
+                                                        '-./
+    """
+
+    art6 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                    \    -
+                     -    '-.
+                      '.    /
+                        '-./
+    """
+
+    art7 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                        \    -
+                         -    '-.
+                          '.    /
+                            '-./
+    """
+
+    art8 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                                \    -
+                                 -    '-.
+                                  '.    /
+                                    '-./
+    """
+
+    art9 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'         
+                                            \    -
+                                             -    '-.
+                                              '.    /
+                                                '-./
+    """
+
+    art10 = r"""
+.-----------------TTTT-----__
+                                         /''''''''''(__O] ----------  __/]
+                      ...---'''"_ --''                                     __@
+                  !'''                   .   __=---------""'''''''''''''''
+                  !                ..--''!   l L !_l   !
+                  !          ..--''      .  /-_j '   '
+                  !    ..--''           /  ,       '   '
+                  !--''                /           `    \
+                                       L'           \    -
+                                                     -    '-.
+                                                      '.    /
+                                                        '-./
+    """
+
+    #Starts the animation 
+
+    print(Fore.RED)
+    print(art1)
+    time.sleep(0.5)
+    clear()
+    print(art2)
+    time.sleep(0.5)
+    clear()
+    print(art1)
+    time.sleep(0.5)
+    clear()
+    print(art2)
+    time.sleep(0.5)
+    clear()
+    print(art1)
+    time.sleep(0.5)
+    clear()
+    print(art2)
+    time.sleep(0.5)
+    clear()
+    print(art1)
+    time.sleep(0.5)
+    clear()
+    print(art3)
+    time.sleep(0.5)
+    clear()
+    print(art4)
+    time.sleep(0.5)
+    clear()
+    print(art5)
+    time.sleep(0.5)
+    clear()
+    print(art6)
+    time.sleep(0.5)
+    clear()
+    print(art7)
+    time.sleep(0.5)
+    clear()
+    print(art8)
+    time.sleep(0.5)
+    clear()
+    print(art9)
+    time.sleep(0.5)
+    clear()
+    print(art10)
+    time.sleep(0.5)
+
+    #Animation end
+    
+    #Same as \n
+    print('')
+    
+    #Inputs
+    print(f'{Fore.GREEN}sv has been summoned.')
+    ip = input(f'{Fore.RED}Ip: ')
+    bytesize = int(input(f'{Fore.YELLOW}Bytes: '))
+    print(Fore.RED)
+
+    #If using high byte size it sends faster to make payload more.
+    if bytesize > 32:
+        #0.01 is extremely fast will ping around 5 times a second
+        sleeptime = 0.01
+    else:
+        #0.3 around normal sleep time
+        sleeptime = 0.3
+        
+    while True:
+        #Sleeps before sending again
+        time.sleep(sleeptime)
+        #Runs ping command using cmd
+        os.system(f'ping -l {bytesize} -w 15 -n 1 {ip} | FIND "TTL="')
+
+def on_open():
+    #Opening msg broken lol
+    msg = f"""
+{Fore.CYAN}┌─┐┬  ┬
+{Fore.CYAN}└─┐└┐┌┘
+{Fore.CYAN}└─┘ └┘ 
+"""
+    print(msg)
+    #Sleeps so u can see message
+    time.sleep(2)
+    #Clear
+    clear()
+
+    #Starts animation function
+    movementart()
+
+#Runs on_open() when opened.
+on_open()
